474bc7bc0ab81db1090ea81ccc6651eb36cb8f84
Yiling-J <njjyl723@gmail.com>
2018-05-07 18:00:47 +0800

    Using simple test

--- admintree/tests.py
+++ admintree/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from .factories import TreeFactory, TreeNodeFactory, TreePathFactory
+from .models import Tree, TreeNode, TreePath
 
 
 class TreeTest(TestCase):
@@ -8,16 +8,16 @@ class TreeTest(TestCase):
     def test_tree(self):
         response = self.client.get("/tree/")
         self.assertEqual(response.status_code, 404)
-        tree = TreeFactory(name='T1')
-        tree2 = TreeFactory(name='T2')
-        node1 = TreeNodeFactory()
-        node2 = TreeNodeFactory()
-        node3 = TreeNodeFactory()
-        node4 = TreeNodeFactory()
-        TreePathFactory(node_from=tree.start_node, node_to=node1, order=0, tree=tree)
-        TreePathFactory(node_from=tree.start_node, node_to=node2, order=1, tree=tree)
-        TreePathFactory(node_from=node1, node_to=node3, order=0, tree=tree)
-        TreePathFactory(node_from=tree2.start_node, node_to=node4, order=0, tree=tree2)
+        tree = Tree.objects.create(name='T1')
+        tree2 = Tree.objects.create(name='T2')
+        node1 = TreeNode.objects.create(title='n1', type='Object')
+        node2 = TreeNode.objects.create(title='n2', type='Object')
+        node3 = TreeNode.objects.create(title='n3', type='Object')
+        node4 = TreeNode.objects.create(title='n4', type='Object')
+        TreePath.objects.create(node_from=tree.start_node, node_to=node1, order=0, tree=tree)
+        TreePath.objects.create(node_from=tree.start_node, node_to=node2, order=1, tree=tree)
+        TreePath.objects.create(node_from=node1, node_to=node3, order=0, tree=tree)
+        TreePath.objects.create(node_from=tree2.start_node, node_to=node4, order=0, tree=tree2)
 
         response = self.client.get("/tree/T1/")
         self.assertEqual(response.status_code, 200)
